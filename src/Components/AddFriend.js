1579715f5813b8c194cfab22fd8e091aac92ed9e
Miranda Cascione <miranda.cascione@gmail.com>
2023-04-21 14:37:16 -0700

    posting new friend inputs to friendsList from /friends/add

--- src/Components/AddFriend.js
+++ src/Components/AddFriend.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddFriend = () => {
+   const navigate = useNavigate();
+   const randomId = generateRandomId();
    
+   const [form, setForm] = useState({
+        id:randomId,    
+        name: '',
+        age: '',
+        email: ''
+   });
+
+   function generateRandomId() {
+        let result = '';
+        const characters = 'abcdefghijklmnopqrstuvwxyz';
+        for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+        }
+        return result;
+    }
+
+   const handleChange = (e) => {
+    setForm({
+        ...form,
+        [e.target.name]: e.target.value
+    })
+   }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        const token = localStorage.getItem('token');
+        axios.post("http://localhost:9000/api/friends", form, {
+            headers: {
+                authorization: token
+            }
+        })
+            .then(() => {
+                navigate("/friends");
+            })
+            .catch(err => {
+                console.log(err);
+            })
     };
    
     return (
@@ -12,15 +52,15 @@ const AddFriend = () => {
             <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">FRIEND NAME:</label>
-                <input id="name"/>
+                <input id="name" name="name" onChange={handleChange}/>
             </div>
             <div>
                 <label htmlFor="age">FRIEND AGE:</label>
-                <input id="age"/>
+                <input id="age"name="age" onChange={handleChange}/>
             </div>
             <div>
                 <label htmlFor="email">FRIEND EMAIL:</label>
-                <input id="email"/>
+                <input id="email" name="email" onChange={handleChange}/>
             </div>
             <button>SUBMIT</button>
         </form>
